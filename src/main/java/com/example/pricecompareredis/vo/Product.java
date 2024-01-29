7195aa1b0d7971292d4a5c3261c1a39b43ac56df
Develing1991 <completed0728@gmail.com>
2024-01-29 17:08:04 +0900

    feat: edit price type int to Long

--- src/main/java/com/example/pricecompareredis/vo/Product.java
+++ src/main/java/com/example/pricecompareredis/vo/Product.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class Product {
+
+    private String prodGrpId;  // FPG0001
     private String productId; // UUID
-    private int price; // 25000 (WON)
+    private double price; // 25000 (WON)
 }
