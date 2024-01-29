a8303c81fa704f00d542b45e1073ec491e7c1c97
Develing1991 <completed0728@gmail.com>
2024-01-29 18:57:47 +0900

    feat: add interface method

--- src/main/java/com/example/pricecompareredis/service/LowestPriceService.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceService.java
@@ -1,10 +1,18 @@
 package com.example.pricecompareredis.service;
 
+import com.example.pricecompareredis.vo.Keyword;
 import com.example.pricecompareredis.vo.Product;
+import com.example.pricecompareredis.vo.ProductGrp;
 
 import java.util.Set;
 
 public interface LowestPriceService {
     Set getZsetValue(String key);
     Long setNewProduct(Product product);
+
+    Long setNewProductGrp(ProductGrp productGrp);
+
+    Long setNewProductKeyword(String keyword, String productGrpId, double score);
+
+    Keyword getLowestPriceProductByKeyword(String keyword);
 }
