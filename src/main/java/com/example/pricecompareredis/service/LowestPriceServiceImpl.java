faea48825d715cd388b24130612c4b3fc71c7541
Develing1991 <completed0728@gmail.com>
2024-01-29 17:07:45 +0900

    feat: add service method newProduct

--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.pricecompareredis.service;
 
+import com.example.pricecompareredis.vo.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,23 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class LowestPriceServiceImpl implements LowestPriceService{
 
-    private  final RedisTemplate myProductPriceRedis;
+    private  final RedisTemplate<String, String> myProductPriceRedis;
 
     @Override
     public Set getZsetValue(String key) {
-        Set myTempSet = new HashSet();
-        myTempSet = myProductPriceRedis.opsForZSet().rangeWithScores(key, 0, 9);
-        return myTempSet;
+        //Set myTempSet = new HashSet();
+        return myProductPriceRedis.opsForZSet().rangeWithScores(key, 0, 9);
     }
+
+    @Override
+    public Long setNewProduct(Product product) {
+
+        myProductPriceRedis.opsForZSet()
+                .add(product.getProdGrpId(), product.getProductId(), product.getPrice());
+
+        return myProductPriceRedis.opsForZSet()
+                .rank(product.getProdGrpId(), product.getProductId());
+    }
+
+
 }
