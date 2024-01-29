bca9f33fb3e0d57b6a04ba5ba3d705c7ae8e74df
Develing1991 <completed0728@gmail.com>
2024-01-29 12:05:52 +0900

    feat: add controller

---
+++ src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
@@ -0,0 +1,22 @@
+package com.example.pricecompareredis.controller;
+
+import com.example.pricecompareredis.service.LowestPriceServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping(path = "/")
+@RequiredArgsConstructor
+public class LowestPriceController {
+
+    private final LowestPriceServiceImpl lowestPriceService;
+
+    @GetMapping("/getZsetValue")
+    public Set getZsetValue(String key){
+        return lowestPriceService.getZsetValue(key);
+    }
+}
