15f507ad20ca943435472fe56bdeddf4daf71684
Develing1991 <completed0728@gmail.com>
2024-01-29 18:58:39 +0900

    feat: add controller methods setNewProductGrp, setNewProductKeyword, getLowestPriceProductByKeyword

--- src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
+++ src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
@@ -1,7 +1,9 @@
 package com.example.pricecompareredis.controller;
 
 import com.example.pricecompareredis.service.LowestPriceServiceImpl;
+import com.example.pricecompareredis.vo.Keyword;
 import com.example.pricecompareredis.vo.Product;
+import com.example.pricecompareredis.vo.ProductGrp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,5 +24,20 @@ public class LowestPriceController {
     @PutMapping(path = "/product")
     public Long setNewProduct(@RequestBody Product product){
         return lowestPriceService.setNewProduct(product);
+    }
+
+    @PutMapping(path = "/productGroup")
+    public Long setNewProductGrp(@RequestBody ProductGrp productGrp){
+        return lowestPriceService.setNewProductGrp(productGrp);
+    }
+
+    @PutMapping(path = "/productGroupToKeyword")
+    public Long setNewProductKeyword(String keyword, String productGrpId, double score){
+        return lowestPriceService.setNewProductKeyword(keyword, productGrpId, score);
+    }
+
+    @GetMapping(path = "/productPrice/lowest")
+    public Keyword getLowestPriceProductByKeyword(String keyword){
+        return lowestPriceService.getLowestPriceProductByKeyword(keyword);
     }
 }
