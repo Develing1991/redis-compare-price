1a3b4f35633abddfa9d678fce73a457bb8bb7d27
Develing1991 <completed0728@gmail.com>
2024-01-29 17:07:18 +0900

    feat: add controller product put method

--- src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
+++ src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
@@ -1,10 +1,9 @@
 package com.example.pricecompareredis.controller;
 
 import com.example.pricecompareredis.service.LowestPriceServiceImpl;
+import com.example.pricecompareredis.vo.Product;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -18,5 +17,10 @@ public class LowestPriceController {
     @GetMapping(path = "/getZsetValue")
     public Set getZsetValue(String key){
         return lowestPriceService.getZsetValue(key);
+    }
+
+    @PutMapping(path = "/product")
+    public Long setNewProduct(@RequestBody Product product){
+        return lowestPriceService.setNewProduct(product);
     }
 }
